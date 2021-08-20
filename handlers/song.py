229ba32ec6c6f034c9709bd3ab4ac36f61ba8625
Talia Turkish Music <83137081+Mehmet5506@users.noreply.github.com>
2021-08-20 15:24:19 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -32,7 +32,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 ғɪɴᴅɪɴɢ sᴏɴɢ...")
+    m = message.reply("🔎 Şarkı bulunuyor...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -52,13 +52,13 @@ def song(client, message):
         m.edit("✘ sᴏɴɢ ɴᴏᴛ ғᴏᴜɴᴅ.\n\nᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴀ ᴠᴀʟɪᴅ sᴏɴɢ ɴᴀᴍᴇ.")
         print(str(e))
         return
-    m.edit("† ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ...")
+    m.edit("† İndiriliyor...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 Uploader @{bn}**"
+        rep = f"**🎧 TaliaMusicProject**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
