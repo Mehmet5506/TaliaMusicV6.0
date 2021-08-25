83c517dd0139534d812733c1ff7f1f3eb26c201f
Talia Turkish Music <83137081+Mehmet5506@users.noreply.github.com>
2021-08-25 11:06:12 +0300

    Update entertainment.py

--- handlers/entertainment.py
+++ handlers/entertainment.py
@@ -7,14 +7,14 @@ from pyrogram import Client
 from config import BOT_USERNAME
 from helpers.filters import command
 
-@Client.on_message(command(["asupan", f"asupan@Efsanestar_bot]))
+@Client.on_message(command(["asupan", f"asupan@Efsanestar_bot"]))
 async def asupan(client, message):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/asupan/ptl").json()
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+        await message.reply_text("`Yanlış giden bir şey var...`")
 
 
 @Client.on_message(command(["wibu", f"wibu@Efsanestar_bot"]))
@@ -24,7 +24,7 @@ async def wibu(client, message):
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+        await message.reply_text("`Yanlış giden birşey var...`")
 
 
 @Client.on_message(command(["chika", f"chika@Efsanestar_bot"]))
@@ -34,7 +34,7 @@ async def chika(client, message):
         results = f"{resp['url']}"
         return await client.send_video(message.chat.id, video=results)
     except Exception:
-        await message.reply_text("`sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ ʟᴏʟ...`")
+        await message.reply_text("`Yanlış giden birşey var...`")
 
 
 @Client.on_message(command(["truth", f"truth@Efsanestar_bot"]))
@@ -44,27 +44,27 @@ async def truth(client, message):
         results = f"{resp['message']}"
         return await message.reply_text(results)
     except Exception:
-        await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...")
+        await message.reply_text("Yanlış giden birşey var...")
 
 
-@Client.on_message(command(["dare", f"dare@{BOT_USERNAME}"]))
+@Client.on_message(command(["dare", f"dare@Efsanestar_bot"]))
 async def dare(client, message):
     try:
         resp = requests.get("https://tede-api.herokuapp.com/api/dare").json()
         results = f"{resp['message']}"
         return await message.reply_text(results)
     except Exception:
-        await message.reply_text("sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ...")
+        await message.reply_text("Yanlış giden birşey var...")
 
 
-@Client.on_message(command(["lyric", f"lyric@{BOT_USERNAME}"]))
+@Client.on_message(command(["lyric", f"lyric@Efsanestar_bot"]))
 async def lirik(_, message):
     try:
         if len(message.command) < 2:
             await message.reply_text("**ɢɪᴠᴇ ᴀ ʟʏʀɪᴄ ɴᴀᴍᴇ ᴛᴏᴏ !**")
             return
         query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("✈ **sᴇᴀʀᴄʜɪɴɢ ʟʏʀɪᴄs...**")
+        rep = await message.reply_text("✈ **Arıyorum lyric...**")
         resp = requests.get(f"https://tede-api.herokuapp.com/api/lirik?l={query}").json()
         result = f"{resp['data']}"
         await rep.edit(result)
